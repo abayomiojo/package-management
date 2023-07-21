f85aba141f3d2e593f21a32087f7189ec661b1d9
abayomiojo <136175906+abayomiojo@users.noreply.github.com>
2023-07-21 08:34:42 -0400

    Update install-tomcat.sh

--- Tomcat-installation/install-tomcat.sh
+++ Tomcat-installation/install-tomcat.sh
@@ -5,17 +5,20 @@
 # https://github.com/LandmakTechnology/package-management/tree/main/Tomcat-installation
 
 # install Java JDK 1.8+ as a pre-requisit for tomcat to run.
+# change hostname to tomcat
 sudo hostnamectl set-hostname tomcat
+sudo su - ec2-user
 cd /opt 
+# install Java JDK 1.8+ as a pre-requisit for tomcat to run.
 sudo yum install git wget -y
 sudo yum install java-1.8.0-openjdk-devel -y
 # install wget unzip packages.
 sudo yum install wget unzip -y
-sudo wget  https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.76/bin/apache-tomcat-9.0.76.zip 
-sudo tar -xvf apache-tomcat-9.0.76.tar.gz
-sudo rm -rf apache-tomcat-9.0.76.tar.gz
+sudo wget  https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.76/bin/apache-tomcat-9.0.76.zip
+sudo unzip apache-tomcat-9.0.76.zip
+sudo rm -rf apache-tomcat-9.0.76.zip
 ### rename tomcat for good naming convention
-sudo mv apache-tomcat-9.0.76 tomcat9
+sudo mv apache-tomcat-9.0.76 tomcat9  
 ### assign executable permissions to the tomcat home directory
 sudo chmod 777 -R /opt/tomcat9
 sudo chown ec2-user -R /opt/tomcat9
@@ -26,6 +29,7 @@ sh /opt/tomcat9/bin/startup.sh
 sudo ln -s /opt/tomcat9/bin/startup.sh /usr/bin/starttomcat
 sudo ln -s /opt/tomcat9/bin/shutdown.sh /usr/bin/stoptomcat
 starttomcat
+
 echo "end on tomcat installation"
 #========
 
